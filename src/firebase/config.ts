aede39ee037b088a699025385746ce3ee47a9d6c
Penguin <hashamcomp@gmail.com>
2026-02-15 23:21:48 +0000

    Set a default bucket

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:846495703607:web:48e98570b17c59738495b2",
   "apiKey": "AIzaSyDolXYL-sKW6HUc8Vq6HNnFRPS0D-2Mwz4",
   "authDomain": "studio-5792028843-a8d4c.firebaseapp.com",
+  "storageBucket": "studio-5792028843-a8d4c.firebasestorage.app",
   "measurementId": "",
   "messagingSenderId": "846495703607"
 };
