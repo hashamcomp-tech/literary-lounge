246dd7c17413a06db4392adc2242fec446e8cdf4
hashamcomp-tech <hashamcomp@gmail.com>
2026-05-14 02:17:12 +0500

    Update index.ts

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -3,3 +3,4 @@
 export { FirebaseProvider, useFirebase, useAuth, useFirestore, useStorage, useUser, useMemoFirebase } from './provider';
 export { useCollection } from './firestore/use-collection';
 export { useDoc } from './firestore/use-doc';
+
