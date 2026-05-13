bf4b2135cb59de74c3cd21432f10265e4abedbf7
hashamcomp-tech <hashamcomp@gmail.com>
2026-05-13 22:04:31 +0000

    Fix Firebase exports and update Next.js 16 config

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,9 +1,21 @@
-import { FirebaseProvider, useFirebase, useAuth, useFirestore, useStorage, useUser, useMemoFirebase } from './provider';
+import {
+  FirebaseProvider,
+  useFirebase,
+  useAuth,
+  useFirestore,
+  useStorage,
+  useUser,
+  useMemoFirebase,
+} from './provider';
+
+import { FirebaseClientProvider } from './client-provider';
+
 import { useCollection } from './firestore/use-collection';
 import { useDoc } from './firestore/use-doc';
 
 export {
   FirebaseProvider,
+  FirebaseClientProvider,
   useFirebase,
   useAuth,
   useFirestore,
